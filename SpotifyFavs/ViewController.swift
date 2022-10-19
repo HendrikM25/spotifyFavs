54376ba91aa2ec685d31a2820737681e53cd3f29
Hendrik Müller <hendrik-mueller@live.de>
2022-10-19 11:11:54 +0200

    Initial Commit

---
+++ SpotifyFavs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifyFavs
+//
+//  Created by Hendrik Müller on 19.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
